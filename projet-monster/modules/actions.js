6da1dffd94ebf9704a233b46f4e1c4c0efd8842c
karim RAHMOUNI <karim.rahmouni@live.fr>
2020-05-05 19:39:01 +0200

    module + let etatAction

---
+++ projet-monster/modules/actions.js
@@ -0,0 +1,23 @@
+/**
+ * 1. des variables privées décrivant l'état du monstre :
+◦ name : le nom du monstre
+◦ life : nombre de point de vie du monstre
+◦ money : l'argent du monstre
+◦ awake : true ou false, indique si le monstre est réveillé ou non,
+ **/
+let etatAction = {
+     name = name,
+    life = life,
+    money = money,
+    awake = awake
+
+}
+
+let name = (function(){
+    return name
+
+})
+console.log(function.name);
+// retourne le nom du monstre
+
+
